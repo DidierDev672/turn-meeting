2a31fb421037b29089f8e8ac92e5342286f6ffb5
Didier Arias <ariasdev672@gmail.com>
2022-06-23 17:12:48 -0500

    Browser of sttegins of components website

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from "./container/Header.vue"
+</script>
+
+<template>
+<Header />
+<router-view></router-view>
+</template>
+
+<style>
+#app {
+  font-family: Quicksand,Averta, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #212529;
+}
+</style>
