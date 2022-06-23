773a066b79b2adc886d42221a955bbf99a24fe81
Didier Arias <ariasdev672@gmail.com>
2022-06-23 17:10:28 -0500

    Components from website

---
+++ src/components/OrdenTurn.vue
@@ -0,0 +1,59 @@
+<template>
+<section>
+    <div class="style-turn">
+        <span><strong :class="{assets: phase, }"> {{ letter_turn }}: </strong> {{ n_turn }}</span>
+        <div :class="{completed: phase, desactive: phase == false}">
+            <span> {{ state_turn }} </span>
+        </div>
+    </div>
+</section>
+</template>
+
+<script>
+import "../styles/ordenturn.css"
+
+import Assets from "../components/Assets.vue"
+export default {
+    name: "orden-turn",
+    props:{
+        id: String,
+        letter_turn: String,
+        n_turn: Number,
+        phase: Boolean,
+    },
+
+    components:{
+        Assets
+    },
+
+    computed:{
+        state_turn(){
+            return this.phase == true ? 'Activo': 'Desastivado'
+        },
+    }
+}
+</script>
+
+<style scoped>
+.completed{
+    background-color: #9ef01a;
+    color: #FFFFFF;
+    font-size: 13px;
+    font-weight: 600;
+    border-radius: 5px;
+    border: none;
+    box-shadow: 2px 2px 2px #9ef01a;
+    padding: 3px;
+}
+
+.desactive{
+    background-color: #ff0a54;
+    color: #FFFFFF;
+    font-size: 13px;
+    font-weight: 600;
+    border-radius: 5px;
+    border: none;
+    box-shadow: 2px 2px 2px #ff477e;
+    padding: 3px;
+}
+</style>
