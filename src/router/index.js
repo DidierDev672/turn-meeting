4977069c0c1dfff481c3a2735a66e7ab169bd2b8
Didier Arias <ariasdev672@gmail.com>
2022-07-25 16:47:55 -0500

    Add new router from home

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,19 @@ import Turn from "../page/Turn.vue"
 import MyTurn from "../page/MyTurn.vue"
 import SignIn from "../page/SingIn.vue"
 import SignUp from "../page/SignUp.vue"
+import AwaitRoom from "../page/AwaitRoom.vue"
 
 import BasicTurn from "../components/BasicTurn.vue"
 
 const routes = [
     {path: '/', component: Home},
+    {path: '/home', component: Home},
     {path: '/turn', component: Turn},
     {path: '/my-turn', component: MyTurn},
+    {path: '/await-room', component: AwaitRoom},
     {path: '/sign-in', component: SignIn},
     {path: '/sign-up', component: SignUp},
-    {path: '/basic-solict/:letter', name: 'basic-turn', component: BasicTurn, porps: true}
+    {path: '/basic-solict/:letter', name: 'basic-turn', component: BasicTurn, props: true}
 ]
 
 const router = createRouter({
