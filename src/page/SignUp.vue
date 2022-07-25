bbb6e2b369997dcc53865a7838fd35e9386c304f
Didier Arias <ariasdev672@gmail.com>
2022-07-25 16:37:08 -0500

    Creation from user with firebase auth

--- src/page/SignUp.vue
+++ src/page/SignUp.vue
@@ -1,34 +1,64 @@
 <template>
-<div class="container">
-    <form class="box-sign-up">
-        <div class="grid-sign-up">
+    <div class="container">
+        <form class="box-sign-up" action="./SignUp.vue" method="post">
+        <label class="title-sign-up">Register User </label>
+        <div class="flex-sign-up">
             <div class="sign-item">
-                <label>Nombre completo</label>
-                <input type="text" class="text-input" placeholder="Nombre completo" />
+            <label>Correo electronico</label>
+            <input
+                type="text"
+                class="text-input"
+                v-model="email"
+                placeholder="example@example.com"
+                pattern="/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/"
+                required
+            />
             </div>
             <div class="sign-item">
-                <label>N° Identificacion o "Cedula"</label>
-                <input type="text" class="text-input" placeholder="N° Identificacion o 'Cedula'" />
-            </div>
-            <div class="sign-item">
-                <label>Telefono</label>
-                <input type="text" class="text-input" placeholder="Telefono" />
-            </div>
-            <div class="sign-item">
-                <label>Correo electronico</label>
-                <input type="text" class="text-input" placeholder="example@example.com" />
+            <label>Contraseña</label>
+            <input
+                type="password"
+                class="text-input"
+                v-model="password_user"
+                pattern="^(?=w*\d)(?=\w*[a-z])\S{8,16}$"
+                placeholder="Contraseña"
+                required
+            />
             </div>
         </div>
         <div class="item-button">
-            <button type="button" class="btn-sign-up">Registrar</button>
+            <input type="submit" class="btn-sign-up" @click="CreateAuth()" value="Registrar" />
         </div>
-    </form>
-</div>
+        </form>
+    </div>
 </template>
 
 <script>
-import "../styles/signup.css"
+import "../styles/signup.css";
+import { mapActions } from "vuex";
 export default {
-    name: 'sign-up'
-}
+    name: "sign-up",
+    data() {
+        return {
+        email: "",
+        password_user: "",
+        };
+    },
+    methods: {
+        ...mapActions("user", ["CreateUser", "RegisterUser"]),
+
+        CreateAuth() {
+            if( this.email !== "" && this.password_user !== ""){
+                this.CreateUser({
+                    email: this.email,
+                    password: this.password_user,
+                }),
+                this.email = '',
+                this.password_user = ''
+            }else{
+                alert("No deben a ver campos vacios!");
+            }
+        },
+    },
+};
 </script>
