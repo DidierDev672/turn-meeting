4e831b624e4a152aab8bb138445e0a591ccce13e
Didier Arias <ariasdev672@gmail.com>
2022-07-25 16:44:20 -0500

    Configaration de firebase 🔥

---
+++ src/api/settings.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAFfjPmycPMBqXODH38AQ4LuQZ6ZnOks6U",
+    authDomain: "turn-meeting.firebaseapp.com",
+    projectId: "turn-meeting",
+    storageBucket: "turn-meeting.appspot.com",
+    messagingSenderId: "222886743233",
+    appId: "1:222886743233:web:a2158e6a088599efddd840",
+    measurementId: "G-KRGB9HBEZ2"
+}
