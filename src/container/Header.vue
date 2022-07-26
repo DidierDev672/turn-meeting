0979d990dfb8f17809ab7dc7c254e8675b9eb3f3
Didier Arias <ariasdev672@gmail.com>
2022-07-26 08:07:50 -0500

    Navigation from design user

--- src/container/Header.vue
+++ src/container/Header.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="container">
     <nav class="box-navbar">
+        <div class="header-header-turn">
+            <router-link to="/" class="list-link title-header-turn">Turn Meeting</router-link>
+        </div>
         <ul class="list-box">
-            <li class="list-item">
-                <router-link to="/" class="list-link">Turn Meeting</router-link>
-            </li>
             <li class="list-item">
                 <router-link to="/" class="list-link">Home</router-link>
             </li>
@@ -19,20 +19,49 @@
             </li>
         </ul>
         <ul class="list-box-right">
-            <li class="list-item">
+            <template v-if="uid !== ''">
+                <header-user :uid="uid" :email="email" :name_full="name_full" />
+            </template>
+            <template v-else>
+                <li class="list-item">
                 <router-link to="/sign-in" class="list-link">Sign In</router-link>
             </li>
             <li class="list-item">
                 <router-link to="/sign-up" class="list-link">Sign Up</router-link>
             </li>
+            </template>
         </ul>
     </nav>
 </div>
 </template>
 
 <script>
-import "../styles/header.css"
+import "../styles/header.css";
+import HeaderUser from "../components/HeaderUser.vue";
+import { auth } from "../api/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 export default {
-    name: 'Header'
+    name: 'Header',
+    data(){
+        return{
+            uid: "",
+            email: "",
+            name_full: ""
+        }
+    },
+
+    components:{
+        HeaderUser
+    },
+
+    created(){
+        onAuthStateChanged(auth, (user) => {
+            if(user){
+                this.uid = user.uid,
+                this.email = user.email,
+                this.name_full = user.name
+            }
+        });
+    }
 }
 </script>
