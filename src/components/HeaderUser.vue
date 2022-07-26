66f270b837eb893cb4f119830eff73807459cdb5
Didier Arias <ariasdev672@gmail.com>
2022-07-26 08:10:54 -0500

    Information from user

---
+++ src/components/HeaderUser.vue
@@ -0,0 +1,28 @@
+<template>
+<section>
+    <select class="form-select-header-user">
+        <option>{{  email }}</option>
+        <option>Editar perfil</option>
+        <option>Cerrar Session</option>
+    </select>
+</section>
+</template>
+
+<script>
+import "../styles/header.css";
+export default {
+    name: "header-user",
+    props:{
+        uid: String,
+        email: String,
+        name_full: String,
+    },
+
+    data(){
+        return{
+            emailC: this.email,
+            name: this.name_full
+        }
+    },
+}
+</script>
