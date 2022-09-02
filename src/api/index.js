24197d7189ae348f7808394700100138b53efc81
DidierDev672 <ariasdev672@gmail.com>
2022-09-02 18:21:04 -0500

    Creating to firebase authation

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,27 @@
 import { db } from "./firebase"
-import { collection, addDoc } from "firebase/firestore"
-const UserRef = "users"
+import { collection, addDoc, updateDoc, doc } from "firebase/firestore"
+const UserRef = "dataProfile";
 
-export default {}
+export default {
+    async EditProfile({ uid, nameUser, phone,email, password }){
+        const docRef = await addDoc(collection(db, UserRef), {
+            uid: uid,
+            nameUser: nameUser,
+            phone: phone,
+            email: email,
+            password: password
+        });
+
+        return docRef.data();
+    },
+
+    async UpdateEditProfile({ id, nameUser, email, phone }){
+        const ProfileRef = doc(db, UserRef, id);
+
+        await updateDoc(ProfileRef, {
+            "nameUser": nameUser,
+            "email": email,
+            "phone": phone
+        });
+    }
+}
